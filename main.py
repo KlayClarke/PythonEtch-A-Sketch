98a44f0ab932d87e19c5efd58d8b5846c5ff610d
Klay Clarke <klayaclarke@gmail.com>
2021-11-16 10:39:34 -0500

    able to sketch using turtle; using wasd commands for movement and c to clear canvas after

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-tim.shape('turtle')
-tim.color('burlywood')
-print(tim)
 
+
+def create_turtle():
+    tim.shape('turtle')
+    tim.color('burlywood')
+    tim.width(9)
+    tim.speed('fastest')
+
+
+create_turtle()
+
+
+def forward():
+    tim.forward(10)
+
+
+def backward():
+    tim.back(10)
+
+
+def turn_left():
+    tim.left(5)
+
+
+def turn_right():
+    tim.right(5)
+
+
+def clear():
+    tim.reset()
+    create_turtle()
+
+
+def move(func):
+    return func
 
 
 my_screen = Screen()
+my_screen.listen()
+my_screen.onkeypress(key='w', fun=move(forward))
+my_screen.onkeypress(key='s', fun=move(backward))
+my_screen.onkeypress(key='a', fun=move(turn_left))
+my_screen.onkeypress(key='d', fun=move(turn_right))
+my_screen.onkeypress(key='c', fun=move(clear))
 my_screen.exitonclick()
